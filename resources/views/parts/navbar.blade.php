149556a011de3141844c47a826fde1ec5e1c904d
Manypton <manypton@gmail.com>
2023-01-14 22:18:30 +0200

    next

---
+++ resources/views/parts/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom: solid 2px #0085FF; border-radius:20px;"  id="navbartop" data-navbar-on-scroll="data-navbar-on-scroll" data-aos="fade-down" data-aos-duration="500">
+    <div class="container"><a class="navbar-brand d-flex align-items-center fw-bolder fs-2" style="max-width: 75%;" href="http://weballday.ro/">
+      <img src="/logo.svg" style="max-width: 100%;">
+      </a>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto pt-2 pt-lg-0 ">
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/">Acasa</a></li>
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/portofoliu.php">Portofoliu</a></li>
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/servicii.php">Ce servicii oferim?</a></li>
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/preturi.php">Cat costa?</a></li>
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/echipa.php">Cine suntem?</a></li>
+          <li class="nav-item px-2"><a class="nav-link text-warning fw-medium f-menu" href="/blog.php">Blog</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
