da48da7cbe29c2f78716fa62dbc0301250c3395f
DanDinFier <122694523+DanDinFier@users.noreply.github.com>
2023-01-14 22:33:09 +0200

    Ok

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//route for projects    
+Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index'])->name('projects.index');
